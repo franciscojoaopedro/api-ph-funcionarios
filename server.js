704c912203f7caf1b149b9296dabd54f19d6ab92
pretohostdev <projectos@dev.petrohost.ao>
2024-04-17 13:53:29 +0100

    FEAD ADD database,contacto

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const routes = require('./src/routes');
-const db = require('./src/config/database');
+const conectarMongoDB = require('./src/config/database');
+
+
 const app = express();
 
 
 require('dotenv').config();
+conectarMongoDB()
 app.use(express.json({limit:"10mb"}));
 app.use(routes);
 
 const PORT=process.env.PORT || 3000
-db.once('open', function() {
-  app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT} e conectado ao MongoDB`);
-  });
-});
+app.listen(PORT,()=>{
+  console.log("SERVER IS RUNNING")
+})
